3fd50a3abbc033d65f102552f4daed08a4801f1f
toby20-meet <toby20@meet.mit.edu>
2018-08-15 17:14:48 +0300

    Stopping here

---
+++ fruit_sorter.py
@@ -0,0 +1,9 @@
+new_fruit = input("What fruit would you like to choose?")
+if "Apple"== new_fruit:
+    print('Bin1')
+elif "Orange" == new_fruit:
+    print('Bin2')
+elif 'Pear' == new_fruit:
+    print('bin3')
+else:
+    print("wtf dis froot?")
